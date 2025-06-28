2dfa8b34702f524297087f43e7d495922edc4926
Jack O'Sullivan <jack@192.168.0.100>
2025-06-28 14:39:20 +0700

    feat: update default welcome scene with new clean input box design and white background

--- src/generated/entities.ts
+++ src/generated/entities.ts
@@ -2,7 +2,7 @@
  * THIS FILE IS AUTO-GENERATED FROM DATABASE SCHEMA
  * DO NOT EDIT MANUALLY - RUN: npm run generate:types
  * 
- * Generated at: 2025-06-27T04:12:52.498Z
+ * Generated at: 2025-06-28T02:05:22.682Z
  */
 
 /**
